2e2c3114cf13743cd37d7dd23f495108d9726d09
Bya Sebastien <byaebastien@hotmail.com>
2026-06-24 15:46:38 +0200

    Initial commit

---
+++ api/src/main/java/be/bstorm/tf_java_2026_rcd_bend/TfJava2026RcdBendApplication.java
@@ -0,0 +1,13 @@
+package be.bstorm.tf_java_2026_rcd_bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfJava2026RcdBendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TfJava2026RcdBendApplication.class, args);
+    }
+
+}
